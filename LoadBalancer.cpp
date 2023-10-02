5dba0e43836f3d46e4fb370d12875197222a18b4
caleb-bynum <Caleb.bynum@tamu.edu>
2023-10-02 17:46:34 -0500

    random generation of requests

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+using namespace std;
 class LoadBalancer {
-    
-}
+    int numRequests;
+    LoadBalancer() {
+        cout << "initialize" << endl;
+        this->numRequests = 0;
+    }
+};
--- Request.cpp
+++ Request.cpp
@@ -1,10 +1,37 @@
+#include <iostream>
 #include <string>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
 class Request {
 public:
     string ipIn;
     string ipOut;
-    int time;
+    int requestTime;
+
+    Request(string ipIn, string ipOut, int time) {
+        this->ipIn = ipIn;
+
+        this->ipOut = ipOut;
+        
+        this->requestTime = time;
+    }
+
+    Request() {
+        this->ipIn = this->GenerateRandomIp();
+
+        this->ipOut = this->GenerateRandomIp();
+
+        this->requestTime = 3 + rand() % 98;
+    }
+
+    string GenerateRandomIp() {
+        srand(time(NULL));
+        int octet1 = rand() % 255;
+        int octet2 = rand() % 255;
+        int octet3 = rand() % 255;
+        int octet4 = rand() % 255;
+
+        return to_string(octet1) + "." + to_string(octet2) + "." + to_string(octet3) + "." + to_string(octet4);
+    }
 };
