58cb12099259f53a7889695f48328286d0f849cf
caleb-bynum <Caleb.bynum@tamu.edu>
2023-10-02 18:00:02 -0500

    removed requestlogic file in favor of loadbalancer

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include "Request.cpp"
 using namespace std;
 class LoadBalancer {
-    int numRequests;
+    queue<Request> requestQueue;
+    int timeTracker;
+    int numServers;
+
     LoadBalancer() {
-        cout << "initialize" << endl;
-        this->numRequests = 0;
+        cout << "Initializing Load Balancer..." << endl;
+        
+        this->requestQueue = queue<Request>();
+        this->timeTracker = 0;
+        this->numServers = 0;
+        
     }
 };
